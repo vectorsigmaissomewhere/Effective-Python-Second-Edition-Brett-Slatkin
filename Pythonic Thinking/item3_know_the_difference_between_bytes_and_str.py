1cd9ee596914eb789d8a0b6f396468448a179497
whereyoucan'tfind <74447376+whocanbevectorsigma@users.noreply.github.com>
2023-12-22 17:09:01 +0545

    Update item3_know_the_difference_between_bytes_and_str.py

--- Pythonic Thinking/item3_know_the_difference_between_bytes_and_str.py
+++ Pythonic Thinking/item3_know_the_difference_between_bytes_and_str.py
@@ -10,3 +10,8 @@ and \x65 means e
 so this this first converted into ascii values 
 and then a is what prints the original bytes object
 """
+#str 
+#bytes
+#   unicode to binary and binary to unicode
+#   encode method of str    decode method which is a method of bytes
+#   you can use your own ending in this methods
