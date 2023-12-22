9b021beed0c4112bad2e86b05160b6857c0d758b
whereyoucan'tfind <74447376+whocanbevectorsigma@users.noreply.github.com>
2023-12-22 17:19:23 +0545

    Update item3_know_the_difference_between_bytes_and_str.py

--- Pythonic Thinking/item3_know_the_difference_between_bytes_and_str.py
+++ Pythonic Thinking/item3_know_the_difference_between_bytes_and_str.py
@@ -26,3 +26,14 @@ def to_str(bytes_or_str):
     return value #Instance of str
 print(repr(to_str(b'foo')))
 print(repr(to_str('bar')))
+
+#This second function takes a bytes or str instance and always returns a str
+def to_str(bytes_or_str):
+    if isinstance(bytes_or_str,bytes):
+        value=bytes_or_str.decode('utf-8')
+    else:
+        value=bytes_or_str
+    return value #Instance of str
+print(repr(to_str(b'foo')))
+print(repr(to_str('bar')))
+
