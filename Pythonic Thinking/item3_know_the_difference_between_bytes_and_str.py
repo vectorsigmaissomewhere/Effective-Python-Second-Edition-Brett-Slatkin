d28b054951618a3e0180565400df0d2de866f47b
whereyoucan'tfind <74447376+whocanbevectorsigma@users.noreply.github.com>
2023-12-22 17:13:31 +0545

    Update item3_know_the_difference_between_bytes_and_str.py

--- Pythonic Thinking/item3_know_the_difference_between_bytes_and_str.py
+++ Pythonic Thinking/item3_know_the_difference_between_bytes_and_str.py
@@ -15,3 +15,14 @@ and then a is what prints the original bytes object
 #   unicode to binary and binary to unicode
 #   encode method of str    decode method which is a method of bytes
 #   you can use your own ending in this methods
+
+
+#This first function takes a bytes or str instance and always returns a str
+def to_str(bytes_or_str):
+    if isinstance(bytes_or_str,bytes):
+        value=bytes_or_str.decode('utf-8')
+    else:
+        value=bytes_or_str
+    return value #Instance of str
+print(repr(to_str(b'foo')))
+print(repr(to_str('bar')))
