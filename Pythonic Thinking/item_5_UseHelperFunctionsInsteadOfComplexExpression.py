da0b7005ed5024459eee2bf9f68a6333206fdd3a
whereyoucanfindme <74447376+vectorsigmaissomewhere@users.noreply.github.com>
2024-04-21 18:23:33 +0545

    Update item_5_UseHelperFunctionsInsteadOfComplexExpression.py

--- Pythonic Thinking/item_5_UseHelperFunctionsInsteadOfComplexExpression.py
+++ Pythonic Thinking/item_5_UseHelperFunctionsInsteadOfComplexExpression.py
@@ -6,12 +6,26 @@ print('first statement')
 my_values = parse_qs('red=5&blue=0&green=',keep_blank_values=True)
 print(repr(my_values))
 
+"""
+OUTPUT
+first statement
+{'red': ['5'], 'blue': ['0'], 'green': ['']}
+"""
+
 # in python any thing being empty or 0 means False
 print('\nsecond statement')
 # we don't have opacity key so we will get None
 print('Red: ',my_values.get('red'))
 print('Green: ',my_values.get('green'))
 print('Opacity: ',my_values.get('opacity'))
+
+"""
+OUTPUT
+second statement
+Red:  ['5']
+Green:  ['']
+Opacity:  None
+"""
 
 # using this trick, in python any thing being empty or 0 means False
 # here green is empty so we get 0 and opacity key is not present
@@ -23,6 +37,15 @@ print(f'Red: {red!r}')
 print(f'Green: {green!r}')
 print(f'Opacity: {opacity!r}')
 
+"""
+OUTPUT
+Third Statement
+Red: '5'
+Green: 0
+Opacity: 0
+"""
+
+
 # The above code that we are seeing is hard to read
 # Now we will us if/else ternary expression to make it more clear
 # in single statement
@@ -31,6 +54,11 @@ red_str = my_values.get('red',[''])
 red = int(red_str[0] if red_str[0] else 0)
 print(red)
 
+"""
+OUTPUT
+Fourth Statement
+5
+"""
 
 # Using the same if else in multiple statement
 print('\nFIfth Statement')
@@ -41,6 +69,11 @@ else:
     green = 0
 print(green)
 
+"""
+OUTPUT
+FIfth Statement
+0
+"""
 
 # to solve this now we will us helper function
 # break complex expression in small pieces
@@ -54,30 +87,10 @@ def get_first_int(values,key,default=0):
 green = get_first_int(my_values,'green')
 print(green)
 
-
 """
-output
-first statement
-{'red': ['5'], 'blue': ['0'], 'green': ['']}
-
-second statement
-Red:  ['5']
-Green:  ['']
-Opacity:  None
-
-Third Statement
-Red: '5'
-Green: 0
-Opacity: 0
-
-Fourth Statement
-5
-
-FIfth Statement
-0
-
+OUTPUT
 Sixth Statement
 0
-
 """
+
 
