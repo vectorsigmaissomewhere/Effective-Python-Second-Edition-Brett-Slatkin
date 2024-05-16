b22509ad2bf53547c5a0484b16478503cb22b851
whereyoucanfindme <74447376+vectorsigmaissomewhere@users.noreply.github.com>
2024-05-16 17:04:47 +0545

    Create closureInterationWithVariableScope.py

---
+++ Functions/closureInterationWithVariableScope.py
@@ -0,0 +1,16 @@
+print("\nProgram number 1")
+def sort_priority(values, group):
+    def helper(x):
+        if x in group:
+            return (0, x)
+        return (1, x)
+    values.sort(key=helper)
+
+numbers = [8, 3, 1, 2, 5, 4, 7, 6]
+group = {2, 3, 5, 7}
+sort_priority(numbers, group)
+print(numbers)
+
+"""
+[2, 3, 5, 7, 1, 4, 6, 8]
+"""
