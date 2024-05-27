4e9344791de66ee88aa77f41fb3cac2fa38dc351
whereyoucanfindme <74447376+vectorsigmaissomewhere@users.noreply.github.com>
2024-05-27 16:24:09 +0545

    Update defineFunctionDecoratorsWithFunctoolswraps.py

--- Functions/defineFunctionDecoratorsWithFunctoolswraps.py
+++ Functions/defineFunctionDecoratorsWithFunctoolswraps.py
@@ -1,3 +1,4 @@
+print("Program number 1")
 def trace(func):
     def wrapper(*args, **kwargs):
         result = func(*args, **kwargs)
@@ -27,3 +28,27 @@ fibonacci((4,), {}) -> 3
 
 print(fibonacci)
 # <function trace.<locals>.wrapper at 0x7cc1915887c0>
+
+
+print("Program number 2")
+import pickle
+from functools import wraps
+
+def trace(func):
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        result = func(*args, **kwargs)
+        print(f'{func.__name__}({args!r}, {kwargs!r}) -> {result!r}')
+        return result
+    return wrapper
+
+@trace
+def fibonacci(n):
+    """Return the n-th Fibonacci number"""
+    if n in (0, 1):
+        return n
+    return fibonacci(n-1) + fibonacci(n-2)
+
+help(fibonacci)
+
+print(pickle.dumps(fibonacci))
